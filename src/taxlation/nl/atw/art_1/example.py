5ca259c7fcf6e7c50bdfc2548dca888f58ac937d
JelleWallenburg <75726418+JelleWallenburg@users.noreply.github.com>
2026-06-06 10:21:39 +0100

    deleted main file and simplified imports

--- src/taxlation/nl/atw/art_1/example.py
+++ src/taxlation/nl/atw/art_1/example.py
@@ -1,9 +1,9 @@
 from datetime import date, timedelta
 
-from taxlation.nl.atw.art_1 import Art_1
+from taxlation.nl import atw
 
 # Voorbeeld los gebruik van Art1: 2025-12-24 startdatum en 72 uur
-art_1_voorbeeld= Art_1(startDatum= date(2025,12,24), wettelijkeTermijn= timedelta(hours=72))
+art_1_voorbeeld= atw.Art_1(startDatum= date(2025,12,24), wettelijkeTermijn= timedelta(hours=72))
 
 print("Verlenging van termijn met Art_1", art_1_voorbeeld.verlengingTermijn)
 print("Einddatum na verlenging:", art_1_voorbeeld.einddatum)
